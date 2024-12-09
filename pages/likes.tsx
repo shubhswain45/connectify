0cf55b75cf5c5ea8104e488430fae77533c49cf5
shubhswain45 <shubhkantswain@.com>
2024-12-09 19:00:31 +0530

    creating dummy controller

--- pages/likes.tsx
+++ pages/likes.tsx
@@ -2,7 +2,6 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { Play } from "lucide-react"; // Lucide React icon for edit
 import MainLayout from "@/layout/MainLayout";
 import Topbar from "@/components/Topbar";
-import PlayButton from "@/components/PlayButton";
 
 // Dummy data for album
 const dummyAlbum = {
